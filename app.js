9e1fcbb2a2a675dee055eefe5c6bbcd6a6fd1f98
richa <richa.verma.17dec@gmail.com>
2019-10-05 15:16:10 +0530

    Added some comments in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const Logger = require('./logger');
-const logger = new Logger();
+const Logger = require('./logger'); //Logger is a class
+const logger = new Logger(); //logger is an object of Logger class
 
 //Register a listener. Listener must be registered before an event is triggered because only registered listeners are called at the trigger of an event
 logger.on("messageLogged", (arg) => {
